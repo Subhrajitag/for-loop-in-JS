2efaa7332bce8b4d6be0f838cbbeba8784c8f70a
Subhrajita <86348041+Subhrajitag@users.noreply.github.com>
2022-02-20 11:07:03 +0000

    1.For-loop in JS

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+//For loop in JS
+
 const nemo= ["nemo"];
 
 function findNemo(array) {
+   let t0=performance.now();
   for (let i = 0; i < array.length; i++) {
+   
     if(array[i]== "nemo")
     {
       console.log("Nemo found");
     }
   }
+  let t1=performance.now();
+  console.log("call to find nemo took " + (t1-t0) + " miliseconds");
 }
 findNemo(nemo);
